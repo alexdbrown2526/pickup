8b269da6d2d21d7509bf2e7ac343a25328913078
Alex Brown <alexdbrown2526@gmail.com>
2022-03-22 17:10:53 -0500

    map loading

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-function App() {
-  return <div></div>;
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33,
+    },
+    zoom: 11,
+  };
+
+  render() {
+    return (
+      <div style={{ height: "100vh", width: "100%" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: process.env.API_KEY }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+        </GoogleMapReact>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default SimpleMap;
